6940fc6d9080a9ae45dfdcc1befa40e67ab44186
Alex Parella <aaparella@ucdavis.edu>
2014-09-21 22:59:12 -0700

    Initial Commit

---
+++ iPrompter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iPrompter
+//
+//  Created by Alex Parella on 9/21/14.
+//  Copyright (c) 2014 Tree Traversal Collective. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iPrompter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iPrompter
+//
+//  Created by Alex Parella on 9/21/14.
+//  Copyright (c) 2014 Tree Traversal Collective. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
